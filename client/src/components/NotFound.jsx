027a4d09670e3a96968c7812d093dba1eeaff94c
Yousae <yousaex24@gmail.com>
2023-03-06 18:51:40 -0600

    commented out ginger bread man img

--- client/src/components/NotFound.jsx
+++ client/src/components/NotFound.jsx
@@ -1,7 +1,7 @@
 const NotFound = () => {
     return (
         <div className="min-h-screen bg-gradient-to-r from-[#89C7AE] to-blue-400 flex justify-center items-center flex-col">
-            <img src="/src/assets/broken.svg" alt="broken ginger bread man" className="w-44"/>
+            {/* <img src="/src/assets/broken.svg" alt="broken ginger bread man" className="w-44"/> */}
             <p className="font-bold text-7xl">404</p>
             <p>This Page Does not Exist</p>
         </div>
